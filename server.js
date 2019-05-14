af6dc2bbba82e80f7922155d13df9caad19990e3
corinnachan <corinnaccm@gmail.com>
2019-05-14 14:44:21 +0100

    expanded fs requests to accommodate multiple assets

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var fs = require ('fs');
+var fs = require('fs');
 
 var message = 'I am so happy to be part of the Node Girls workshop!';
 var nodeMessage = 'This is the message for Node endpoint!';
@@ -15,29 +15,35 @@ function handler (request, response) {
 
   if (endpoint === '/') {
     response.writeHead(200, {'Content-type': 'text/html'});
-
     fs.readFile(__dirname + '/public/index.html', function (error, file) {
       if (error) {
         console.log(error);
         return;
       }
       response.end(file);
-    })
+    });
+  } else if (endpoint === '/node') {
+    response.writeHead(200, {'Content-type': 'text/html'});
+    response.write(nodeMessage);
+    response.end();
+
+  } else if (endpoint === '/girls') {
+    response.writeHead(200, {'Content-type': 'text/html'});
+    response.write(girlsMessage);
+    response.end();
+
+  } else {
+    response.writeHead(200);
+    fs.readFile(__dirname + '/public/' + endpoint, function (error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
   }
-  //
-  // response.writeHead(200, {'Content-Type': 'text/html'});
-  //
-  // if (endpoint === '/node') {
-  //   // response.writeHead(200, {'Content-Type': 'text/html'});
-  //   response.write(nodeMessage);
-  //   // response.end();
-  // } else if (endpoint === '/girls') {
-  //   response.write(girlsMessage);
-  // } else {
-  //   response.write(message);
-  // }
-  //
-  // response.end();
+
+
 }
 
 var server = http.createServer(handler);
